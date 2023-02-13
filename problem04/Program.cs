b184fc7a89b2e6db9e75503f2859ba2a0f7cf475
ezz2003 <ezz2003@yandex.ru>
2023-02-13 11:15:51 +1000

    solved the problem №4

---
+++ problem04/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 4: Напишите программу, которая принимает на
+// вход три числа и выдаёт максимальное из этих чисел.
+
+// --------  Вариант 1 -------------
+// Console.Write("Введите первое число: ");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// int number2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите третье число: ");
+// int number3 = Convert.ToInt32(Console.ReadLine());
+// if (number2 > number1)
+// {
+  // number1 = number2;
+// }
+// if (number3 > number1)
+// {
+  // number1 = number3;
+// }
+// Console.WriteLine($"Наибольшее из чисел - {number1}");
+// 
+
+// ------------- Вариант2 --------------------
+Console.Write("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+int max = number1;
+if (number2 > max)
+{
+  max = number2;
+}
+if (number3 > max)
+{
+  max = number3;
+}
+Console.WriteLine($"Наибольшим из чисел {number1}, {number2}, {number3} является {max}");
